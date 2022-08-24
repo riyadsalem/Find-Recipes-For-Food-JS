4a0f02836d8482b0ea1a08dea05bbe75fd5f6cc5
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-08-24 15:00:43 +0300

    #3 Create try...catch for fetch api Release #3

--- src/js/controller.js
+++ src/js/controller.js
@@ -17,10 +17,12 @@ const showRecipe = async function () {
       `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
     );
     const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
     console.log(data);
   } catch (err) {
     alert(err);
-    throw err;
   }
 };
 
