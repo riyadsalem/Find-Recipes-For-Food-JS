3dad9678bb0604beaa99223da8f2d24489783134
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-08-24 14:53:03 +0300

    #3 Create fetch fucntion >> GET API For Recipe Release #3

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,17 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    alert(err);
+    throw err;
+  }
+};
+
+showRecipe();
