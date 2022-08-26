cd57424ba157eb7e111bb53bb246d3cb831fa4fd
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-08-26 09:38:33 +0300

    #11 Add config.js >>> export variables Release #11

---
+++ src/js/helpers.js
@@ -0,0 +1,13 @@
+import { reject } from 'core-js/fn/promise';
+import { async } from 'regenerator-runtime';
+
+export const getJSON = async function (url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
